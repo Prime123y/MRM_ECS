531fe04c7b20601a05a51ce68ab8ab27af54a5a5
Sanjit Roy <128285153+Prime123y@users.noreply.github.com>
2023-03-19 23:45:33 +0530

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -1,50 +1,29 @@
 import cmath
+poly1='+2x^2-5x-3' #Enter quadratic with appropriate sign
+a=poly1[0:2]
+b=poly1[5:7]
+c=poly1[8:]
 
-def quad(sent): 
+a1=a.split('x')
+b1=b.split("x")
+c1=c.split()
+for x in a1:    
+ a2=eval(x)
+ for x in b1:
+  b2=eval(x)
+  for x in c1:
+   c2=eval(x)
 
-   list1=sent.split()
-   a=0
-   b=0
-   c=0
-   ctr=1
-   x1=0
-   x2=0
-   for x in list1:
-    #print(list1[0])
-    if(x.isdigit()==True):
-        if(ctr==1):
-            a=x
-            ctr+=1
-        elif(ctr==2):
-            b=x
-            ctr+=1
-        else:
-            c=x
-            ctr+=1
-    
-# Converting str values to int
-   a1=int(a)
-   b1=int(b)
-   c1=int(c)
-   check=type(b)
-#Finding Discriminant
-   d=(b1**2) - (4*a1*c1)
-
-   if(d>0):
-     x1= (-b1 + cmath.sqrt(d))/(2*a1)
-     x2= (-b1 - cmath.sqrt(d))/(2*a1)
-   elif(d<0):
-    x1=(-b1/2*a1) + cmath.sqrt(-d)/(2*a1)
-    x2=(-b1/2*a1) - cmath.sqrt(-d)/(2*a1)
-   else:
-    x1=x2=(-b1/2*a1)
-
-   print("{:.2f}".format(x1))
-   print("{:.2f}".format(x2))
-
-sent=quad("- 1 x^2 + 4 x + 3 ")
+d=(b2**2) + (4*a2*c2 )
+if(d>0):
+     x1= (-b2 + cmath.sqrt(d))/(2*a2)
+     x2= (-b2 - cmath.sqrt(d))/(2*a2)
+elif(d<0):
+    x1=(-b2/2*a2) + cmath.sqrt(-d)/(2*a2)
+    x2=(-b2/2*a2) - cmath.sqrt(-d)/(2*a2)
+else:
+    x1=x2=(-b2/2*a2)
 
 
-
-      
-
+print("{:.2f}".format(x1))
+print("{:.2f}".format(x2))
