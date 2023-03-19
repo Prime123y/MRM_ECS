6a939e960df777737e2e5025ba8aae047de9edd4
Sanjit Roy <128285153+Prime123y@users.noreply.github.com>
2023-03-19 23:04:52 +0530

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -1,11 +1,23 @@
 
-list1=[1,2,3,4,1,2,3,6,8,'a','a','b','c']
+list1=[]
+
+while True:
+    value = input("Enter a value, press enter to stop ")
+    if value == "":
+        break
+    list1.append(value)
 
 list2=[]
+l=len(list1)
 for x in list1:
-    if x not in list2:
-        list2.append(x)
-    else:
+     if(x[0]==' '):
+           continue
+        
+     if x.strip() not in list2:
+         list2.append(x.strip())
+     else:
         continue
         
 print(list2)
+
+list1=[]
